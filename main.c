b00c1c28cdc7b747aaec33d013797758bccf724b
jmyanez <jmyanez@miners.utep.edu>
2018-09-19 16:54:06 -0600

    Finished Rectangles Lab (Lab #2)

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
+/*This program will ask for the coordanates of the center of two rectangles
+ * the Height and the Width. Then the program will conclude if those two
+ * rectangles overlap or not.
+ * This program is Lab 2 for the class CS 1320 given by Dr Urenda
+ * Made by Jose M Yanez on 9/19/2018 :)
+*/
 #include <stdio.h>
 #include <stdbool.h>
-#include <sys/param.h>
-#include <w32api/minmax.h>
 
 bool rectangleOverlap(){
     //Declaration of rectangles center height and coordinates
     //Rectangle #1
-    float R1CenterX = 0, R1CenterY=0, R1Height = 1, R1Width =1;
+    float R1CenterX,R1CenterY,R1Height,R1Width;
+    printf("Enter Rectangle 1, Center X coordinate:\n");
+    scanf("%f",&R1CenterX);
+    printf("Enter Rectangle 1, Center Y coordinate:\n");
+    scanf("%f",&R1CenterY);
+    printf("Enter Rectangle 1, Height:\n");
+    scanf("%f",&R1Height);
+    printf("Enter Rectangle 1, Width:\n");
+    scanf("%f",&R1Width);
+
     double R1Bx= R1CenterX + (R1Width/2);
     double R1By = R1CenterY + (R1Height/2);
     double R1Cx= R1CenterX - (R1Width/2);
     double R1Cy = R1CenterY - (R1Height/2);
 
-
     //Rectangle #2 we only use bottom left and top right vertices
-    float R2CenterX = 1, R2CenterY=1, R2Height = 2, R2Width =3;
+    float R2CenterX, R2CenterY, R2Height, R2Width;
+    printf("Enter Rectangle 2, Center X coordinate:\n");
+    scanf("%f",&R2CenterX);
+    printf("Enter Rectangle 2, Center Y coordinate:\n");
+    scanf("%f",&R2CenterY);
+    printf("Enter Rectangle 2, Height:\n");
+    scanf("%f",&R2Height);
+    printf("Enter Rectangle 2, Width:\n");
+    scanf("%f",&R2Width);
+
     double R2Bx= R2CenterX + (R2Width/2);
     double R2By = R2CenterY + (R2Height/2);
     double R2Cx= R2CenterX - (R2Width/2);
@@ -22,36 +43,27 @@ bool rectangleOverlap(){
 
 
     if(R1Cx < R2Bx && R2Cx < R1Bx && R1Cy<R2By && R2Cy < R1By ){
-        printf("Rectangles do Overlap!!\n");
-        double newRectCx = max(R1Cx,R2Cx);
-        double newRectCy = max(R1Cy,R2Cy);
-        double newRectBx = max(R1Bx,R2Bx);
-        double newRectBy = max(R1By,R2By);
-        double rectangleMidpointX = ((R1Cx + R2Cx)/2);
-        double rectangleMidpointY = ((R1Cy + R2Cy)/2);
-        printf("%d\n",newRectCx);
         return true;
     }
     else if(R2Cx < R1Bx && R1Cx < R2Bx && R1Cy<R2By && R2Cy < R1By){
-        printf("Rectangles do Overlap!\n");
         return true;
     }
     else if(R2Cx < R1Bx && R1Cx < R2Bx && R2Cy<R1By && R2By < R1Cy) {
-        printf("Rectangles do Overlap!!!");
         return true;
     }
     else if(R1Cx < R2Bx && R2Cx < R1Bx && R2Cy<R1By && R2By < R1Cy) {
-        printf("Rectangles do Overlap!!!");
         return true;
     }
-    else printf("Rectangles do not Overlap!!!");
+    else
+        return false;
 
 }
 
-
-
-
 int main() {
-    rectangleOverlap();
+    if(rectangleOverlap()){
+        printf("They do Overlap!!\n");
+    }
+    else
+        printf("Nope\n");
     return 0;
 }
